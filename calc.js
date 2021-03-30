ab928b14fe08e7df0352a037b7cc0467143f664c
vitalii-b93 <55174359+vitalii-b93@users.noreply.github.com>
2021-03-30 16:19:26 +0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+
+function clean() {
+    document.form.textview.value = "";
+}
+
+function back() {
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length-1);
+}
+
+function summ() {
+    var result = document.form.textview.value;
+
+    if(result) {
+        document.form.textview.value = eval(result);
+    }
+   
+}
